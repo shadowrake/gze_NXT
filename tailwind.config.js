25e9c6ae9bb6d3ce759284ee071c556bb2605986
Herman Kristiansen <gghh@live.no>
2023-10-26 00:40:44 +0200

    change the gray

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,33 @@ export default withMT({
   ],
   theme: {
     extend: {
+      colors: {
+        sky: {
+          50: "#f0f9ff",
+          100: "#e0f2fe",
+          200: "#bae6fd",
+          300: "#7dd3fc",
+          400: "#38bdf8",
+          500: "#0ea5e9",
+          600: "#0284c7",
+          700: "#0369a1",
+          800: "#075985",
+          900: "#0c4a6e",
+        },
+        gray: {
+          50: "#f9fafb",
+          100: "#f4f5f7",
+          200: "#e5e7eb",
+          300: "#d2d6dc",
+          400: "#9fa6b2",
+          500: "#6b7280",
+          600: "#4b5563",
+          700: "#374151",
+          800: "#1f2937",
+          900: "#111827",
+          950: "#030712",
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
