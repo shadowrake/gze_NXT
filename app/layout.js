c38e60977ea527880a10dd1bcf92fabbe0018378
Herman Kristiansen <gghh@live.no>
2023-11-07 23:46:32 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'GreenZoneEsports | Esports organization',
-  description: 'We are an Esports organization, our staff is a friend group who are passionate about Esports.We have a goal to recruit new/amateur players who are willing to be a part of our organization and try to achive greatness.',
+  description: 'We are a devoted Esports organization, founded by a collective of individuals who are deeply passionate about the world of Esports. Our mission is to scout and recruit emerging talents who are willing to affiliate with our organization and aim for extraordinary success. Our primary plan is to assemble robust teams for Counter-Strike 2 (CS2), Rainbow Six (R6), and League of Legends (LoL).Our ultimate goal is to elevate our organization to a level where we can compete at the pinnacle of the Esports industry.',
   image: 'https://cdn.discordapp.com/attachments/808597548156942356/808597579608367636/gze.png',
   url: 'https://greenzoneesports.com',
   type: 'website',
