d22e64619b89fc667b0ffe67e4731993530f687e
Herman Kristiansen <gghh@live.no>
2023-11-07 23:45:20 +0100

    keywords

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,17 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'GreenZoneEsports | Esports organization',
   description: 'We are an Esports organization, our staff is a friend group who are passionate about Esports.We have a goal to recruit new/amateur players who are willing to be a part of our organization and try to achive greatness.',
+  image: 'https://cdn.discordapp.com/attachments/808597548156942356/808597579608367636/gze.png',
+  url: 'https://greenzoneesports.com',
+  type: 'website',
+  siteName: 'GreenZone Esports',
+  siteLanguage: 'en',
+  siteLocale: 'en_US',
+  twitterUsername: '@GreenZoneEsport',
+  authorName: 'GreenZone Esports',
+  authorUrl: 'https://greenzoneesports.com',
+  metadata: "GreenZone Esports, Esports, Gaming, Gaming organization, Esports organization, GreenZone, norwegian, Norwegian, CS2, LoL, Counter strike 2, Leauge of legends",
+  keywords: "GreenZone Esports, Esports, Gaming, Gaming organization, Esports organization, GreenZone, norwegian, Norwegian, CS2, LoL, Counter strike 2, Leauge of legends",
 }
 
 export default function RootLayout({ children }) {
