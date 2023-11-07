5e11ba2b6bfae210fb0afadd61d5e39abf09b281
Herman Kristiansen <gghh@live.no>
2023-11-08 00:04:07 +0100

    Update contact.tsx

--- app/recruitment/games/[id]/contact.tsx
+++ app/recruitment/games/[id]/contact.tsx
@@ -163,7 +163,7 @@ export default function Contact({ input, team }: Input)  {
 
                 <button
                   className="mt-6 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  disabled={isSubmitting || !isVerified}
+                  disabled={isSubmitting}
                 >
                   {isSubmitting ? "Submitting..." : "Submit"}
                 </button>
