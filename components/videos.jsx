5847c9364c26104f201be52766440b1a92e27494
Herman Kristiansen <gghh@live.no>
2023-11-12 12:48:14 +0100

    Update videos.jsx

--- components/videos.jsx
+++ components/videos.jsx
@@ -6,7 +6,9 @@ export default async function VideoMedia() {
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <div className="flex items-center justify-between space-x-4">
+          <div className="mx-auto max-w-2xl text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Videos from GZE staff and players</h2>
+            </div>
           </div>
           {Object.keys(videoMedia).length != 0 ?
           <div className="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
